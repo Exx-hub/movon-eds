a69ef94a91c8323485e7358b545ad74e7e0533f3
MakaBravo2 <bravo2@makatodatech.com>
2020-07-07 00:05:04 +0800

    fix shedule list

--- src/component/scheduledTrips/list.js
+++ src/component/scheduledTrips/list.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './scheduletrips.scss';
-import {Pagination, Row, Col, Card} from 'antd';
+import {Pagination} from 'antd';
 import pin from '../../assets/destinationpin.png'
 import moment from 'moment';
 
@@ -12,12 +12,7 @@ export const ScheduledTrips = (props)=>{
                     <h2>Select Trip</h2>
                     <span>{moment().format("hh:ss A - MMM DD, YYYY")}</span>
                 </div>
-                <DetailsView key={1} onClick={()=>props.onSelect(0)}/>
-                <DetailsView key={2} onClick={()=>props.onSelect(1)}/>
-                <DetailsView key={3} onClick={()=>props.onSelect(2)}/>
-                <DetailsView key={4} onClick={()=>props.onSelect(3)}/>
-                <DetailsView key={5} onClick={()=>props.onSelect(4)}/>
-                <DetailsView key={6} onClick={()=>props.onSelect(5)}/>
+                <DetailsView {...props} key={1} onClick={()=>props.onSelect(0)}/>
             </div>
         
             <div className="pagination-container">
@@ -32,30 +27,58 @@ export const ScheduledTrips = (props)=>{
 
 const DetailsView = (props) =>{
     return(<div className="details-view-item" onClick={props.onClick || null}>
-        <Row>
-            <Col span={1} style={{height:'100px'}}>
-                <img style={{height:'100px'}} src={pin} />
-            </Col>
-            <Col span={4} style={{height:'100px'}}>
-                <div className="list-content">
-                    <span style={{fontSize:'20px', fontWeight:'400', color:'#fff'}}> Naga</span>   
-                    <span style={{fontSize:'20px', fontWeight:'400', color:'#fff'}}> Cubao</span>   
+    
+            <div className="content-container">
+                <div className="pin-origin-destination">
+                    <img className="pin-icon" src={pin} alt="pin-icon"/>
+                    <div className="origin-destination">
+                        <span style={{flexGrow:1}}>Naga</span>
+                        <span>Cubao</span>
+                    </div>
                 </div>
-            </Col>
-            <Col offset={2} span={4} style={{height:'100px'}}>
-                <div className="list-content">
-                    <span style={{fontSize:'20px', fontWeight:'100', color:'#fff'}} > Bus Model</span>   
-                    <span style={{fontSize:'20px', fontWeight:'100', color:'#fff'}}> Company</span>   
+                <div className="content-text-info">
+                    <div className="content-text-info-items">
+                        <div className="items">
+                            <span className="company-text">Company</span>
+                            <span className="bus-model-text">Bus Model</span>
+                            <span className="departure-time-title">Departure Time</span>
+                            <span className="departure-time">02:52 AM - Jun 30, 2020</span>
+                        </div>
+                    </div>
                 </div>
-            </Col>
-            <Col offset={2} style={{height:'100px'}}>
-                <div className="list-content">
-                    <span style={{fontSize:'20px', fontWeight:'100', color:'#fff'}}> Departure Time</span>   
-                    <span style={{fontSize:'30px', fontWeight:'100', color:'#fff'}}> 02:52 AM - Jun 30, 2020</span>   
-                </div>
-            </Col>
-        </Row>
-       
-
+            </div>
     </div>)
 }
+
+// <div style={{display:'flex', flexDirection:'row'}}>
+//                 <div style={{display:'flex', height:'100px'}}>
+//                     <img style={{height:'100px', position:'absolute'}} src={pin} />
+//                     <div 
+//                         style={{
+//                             marginLeft:'2rem',
+//                             display:'flex', 
+//                             flexDirection:'column',
+//                             justifyContent:'flex-start',
+//                             alignItems:'flex-start',
+//                             height:'100%',
+//                         }}>
+//                         <span style={{flexGrow:1}}>Naga</span>
+//                         <span>Cubao</span>
+//                     </div>
+//                 </div>
+//                 <div style={{
+//                     display:'flex',
+//                     flexDirection:'column',
+//                     justifyContent:'flex-start',
+//                     width:'100%'
+//                 }}>
+//                     <div style={{marginLeft:'10%', height:'100%', display:'flex', flexDirection:' row'}}>
+//                         <div style={{display:'flex', flexDirection:'column', alignSelf:'flex-end'}}>
+//                             <span>Company</span>
+//                             <span style={{marginBottom:'.5rem'}}>Bus Model</span>
+//                             <span>Departure Time</span>
+//                             <span>02:52 AM - Jun 30, 2020</span>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
