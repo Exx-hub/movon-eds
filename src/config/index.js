1f3ddb6e53ec60db070cbdb6a28c49c42c090a1e
ronnie.tayag@movon.com.ph <ronnie.tayag@movon.com.ph>
2020-11-29 14:57:05 +0800

    add build number

--- src/config/index.js
+++ src/config/index.js
@@ -33,7 +33,7 @@ export const config = {
     },
     version: {
         environment:"",
-        build: '1.2.7(30)'
+        build: '1.2.7(31)'
     },
     changeLogs: `removed merchant's copy of fivestar stickers, improve ui, add version, per accounts, add super user filtering`
 
