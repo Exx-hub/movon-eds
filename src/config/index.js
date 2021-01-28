5aafef28b5335362d61d6a09a8bf475889c1ff64
Movon <movon@MacBook-Pro.local>
2021-01-29 00:00:41 +0800

    update build

--- src/config/index.js
+++ src/config/index.js
@@ -33,9 +33,9 @@ export const config = {
     },
     version: {
         environment:"",
-        build: '1.5.1(90)'
+        build: '1.5.1(91)'
     },
-    changeLogs: `fix BI quantity multiply also declared value fee`
+    changeLogs: `allow 0 to input box`
 }
 
 export const ERROR_CODES = {
