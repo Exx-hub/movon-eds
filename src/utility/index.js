819d22c3d61b45efb2e69337ab5a9244290f68cd
Movon <movon@MacBook-Pro.local>
2021-02-01 18:36:05 +0800

    change alter path

--- src/utility/index.js
+++ src/utility/index.js
@@ -60,8 +60,8 @@ const debounce = (func, wait) => {
 };
 
 const alterPath = (path, props) =>{
-  //return process.env.NODE_ENV === 'development' ? '/staging' + path : path;
-  return path;
+  return process.env.NODE_ENV === 'development' ? '/cargo' + path : path;
+  //return path;
 }
 
 const modifyName = fullName =>{
