04c9b75a73328c8ce023ef3efff718b00675c36b
Movon <movon@MacBook-Pro.local>
2021-03-05 10:37:52 +0800

    add verstion

--- src/config/index.js
+++ src/config/index.js
@@ -32,7 +32,7 @@ export const config = {
     }, 
     version: {
         environment:"",
-        build: '1.7.0(114)'
+        build: '1.7.0(115)'
     },
     environment:"staging",
     changeLogs: `DP-556`
