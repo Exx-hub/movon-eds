fd6ff9f744ccee86ac387736c2d5e0e512e67408
Rene Bigay <louie.naraja@movon.com.ph>
2021-03-10 17:38:10 +0800

    1.7.1(121) DP-582

--- src/page/priceMatrix/container/modal.matrix.bicolisarog.weightRange.js
+++ src/page/priceMatrix/container/modal.matrix.bicolisarog.weightRange.js
@@ -14,9 +14,9 @@ function MatrixModalContent(props) {
             renderItem={e => {
                 return (
                     <List.Item>
-                        <div style={{ display: 'flex', width: '100%', alignItems:'center', justifyContent: 'space-between' }}>
+                        <div style={{ display: 'flex', width: '100%', alignItems:'center',justifyContent: 'space-between' }}>
                             <div style={{display:'flex'}}>
-                                <div style={{ marginRight:'2rem', display:'flex', flexDirection:'column', minWidth:'50px', alignItems:'center', justifyContent:'center'}}>
+                                <div style={{ marginRight:'2rem', display:'flex', textAlign:'left',flexDirection:'column', minWidth:'50px', alignItems:'left', justifyContent:'center'}}>
                                    
                                    <Space direction="horizontal">
                                         <span style={{ color:'orange', fontSize: 17, fontWeight: 'bold',  }}>{e.weight1} - </span> 
@@ -26,13 +26,23 @@ function MatrixModalContent(props) {
                                     <span style={{ fontSize: 8, fontWeight: 'bold' }}>Allowable Weight</span> 
                                 </div>
                             </div>
+                            <div style={{display:'flex', width:'40%', justifyContent:'space-between',textAlign:'right'}}>
                             <div style={{display:'flex'}}>
-                                <div style={{ display:'flex', flexDirection:'column', minWidth:'50px', alignItems:'center', justifyContent:'center'}}>
+                                <div style={{ display:'flex', flexDirection:'column', alignItems:'right' ,minWidth:'65px', justifyContent:'center'}}>
                                    
-                                   <span style={{ color:'green', fontSize: 17, fontWeight: 'bold',  }}>₱ {Number(e.excessRate).toFixed(2)}</span> 
-                                   <span style={{ fontSize: 8, fontWeight: 'bold' }}>Kilo Rate</span> 
+                                   <span style={{ color:'green', fontSize: 17, fontWeight: 'bold'}}>₱{Number(e.amount).toFixed(2)}</span> 
+                                   <span style={{ fontSize: 8, fontWeight: 'bold'}}>Amount</span> 
                                    
                                </div>
+                            </div>
+                            <div style={{display:'flex'}}>
+                                <div style={{ display:'flex', flexDirection:'column', alignItems:'right' ,minWidth:'50px', justifyContent:'center'}}>
+                                   
+                                   <span style={{ color:'green', fontSize: 17, fontWeight: 'bold'}}>₱{Number(e.kiloRate).toFixed(2)}</span> 
+                                   <span style={{ fontSize: 8, fontWeight: 'bold'}}>Kilo Rate</span> 
+                                   
+                               </div>
+                            </div>
                             </div>
                         </div>
                     </List.Item>)
