25723a821afe0bc3c336d4165e5840161da303e1
Louie Naraja <louie.naraja@movon.com.ph>
2021-03-18 09:30:56 +0800

    1.7.3(123) EDS-18

--- src/config/index.js
+++ src/config/index.js
@@ -32,10 +32,10 @@ export const config = {
     }, 
     version: {
         environment:"staging",
-        build: '1.7.3(122)'
+        build: '1.7.3(123)'
     },
     environment:"staging",
-    changeLogs: `updated cashier name in create parcel page`
+    changeLogs: `updated textColor switch case in header`
 }
 
 export const ERROR_CODES = {
