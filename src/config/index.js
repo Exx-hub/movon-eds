ede7e61aa846401ceaa5efd2fd40aeec6caf198d
Rene Bigay <louie.naraja@movon.com.ph>
2021-03-25 12:57:44 +0800

    updated develop conflict

--- src/config/index.js
+++ src/config/index.js
@@ -32,7 +32,7 @@ export const config = {
     }, 
     version: {
         environment:"staging",
-        build: '1.7.3(125)'
+        build: '1.7.3(126)'
     },
     environment:"staging",
     changeLogs: `disabled download button`
