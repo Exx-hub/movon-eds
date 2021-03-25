79148e0ab5ddf9a9f90f32c64b4f0e602bef07e9
ronnie tayag <ronnie.tayag@movon.com.ph>
2021-03-25 12:36:42 +0800

    update version

--- src/config/index.js
+++ src/config/index.js
@@ -32,7 +32,7 @@ export const config = {
     }, 
     version: {
         environment:"staging",
-        build: '1.7.3(124)'
+        build: '1.7.3(125)'
     },
     environment:"staging",
     changeLogs: `updated change password`
