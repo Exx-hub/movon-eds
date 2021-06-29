a7eea0a38abc0a5947a05044db91245c6ac026ac
Alvin Acosta <alvin.acosta@movon.com.ph>
2021-06-29 15:36:45 +0800

    add comments and code cleanup

--- src/component/modal/index.js
+++ src/component/modal/index.js
@@ -1,29 +1,35 @@
-import React from 'react';
+import React from "react";
 
-import PromptModal from './promptmodal';
-import MatrixModal from './matrixModal'
-import CustomModal from './matrixModal'
-import DefaultMatrixModal from './defaultModal'
+import PromptModal from "./promptmodal";
+import MatrixModal from "./matrixModal";
+import CustomModal from "./matrixModal";
+import DefaultMatrixModal from "./defaultModal";
 import User from "../../service/User";
-import { UserProfile } from '../../utility';
+import { UserProfile } from "../../utility";
 
-function LogoutModal(props){
-    return <PromptModal
-        visible={props.visible}
-        title="Are you sure you want to log out?"
-        message="Changes you made may not be saved."
-        buttonType="danger"
-        action="Logout"
-        handleCancel={() => {
-          props.handleCancel()
-        }}
-        handleOk={() => { 
-          UserProfile.logout(User);
-          props.history.push("/");
-        }}
-      />
+function LogoutModal(props) {
+  return (
+    <PromptModal
+      visible={props.visible}
+      title="Are you sure you want to log out?"
+      message="Changes you made may not be saved."
+      buttonType="danger"
+      action="Logout"
+      handleCancel={() => {
+        props.handleCancel();
+      }}
+      handleOk={() => {
+        UserProfile.logout(User);
+        props.history.push("/");
+      }}
+    />
+  );
 }
 
 export {
-    PromptModal, MatrixModal, CustomModal, DefaultMatrixModal, LogoutModal
-}
+  PromptModal,
+  MatrixModal,
+  CustomModal,
+  DefaultMatrixModal,
+  LogoutModal,
+};
