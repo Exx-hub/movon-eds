4548770e62b1d8064f4a56e073d0fb91b32d59c0
Alvin Acosta <alvin.acosta@movon.com.ph>
2021-07-01 11:59:13 +0800

    format some lines of code

--- src/component/modal/promptmodal.js
+++ src/component/modal/promptmodal.js
@@ -1,40 +1,44 @@
-import React from 'react'
-import {Modal, Button} from 'antd'
-import PropTypes from 'prop-types';
-import './modal.scss'
+import React from "react";
+import { Modal, Button } from "antd";
+import PropTypes from "prop-types";
+import "./modal.scss";
 
-export default class PromptModal extends React.Component{
-
-    render(){
-      return(
-        <Modal
-          closable = {false}
-          title={this.props.title}
-          visible={this.props.visible}
-          footer={[
-            <Button
-              key="back"
-              onClick={()=>this.props.handleCancel()}
-            >
-              Cancel
-            </Button>,
-            <Button
-              id="voidSubmit"
-              key="submit"
-              type={this.props.buttonType}
-              onClick={()=>this.props.handleOk()}
-              disabled={this.props.disabled}
-            >
+export default class PromptModal extends React.Component {
+  render() {
+    return (
+      <Modal
+        closable={false}
+        title={this.props.title}
+        visible={this.props.visible}
+        footer={[
+          <Button key="back" onClick={() => this.props.handleCancel()}>
+            Cancel
+          </Button>,
+          <Button
+            id="voidSubmit"
+            key="submit"
+            type={this.props.buttonType}
+            onClick={() => this.props.handleOk()}
+            disabled={this.props.disabled}
+          >
             <p>{this.props.action}</p>
-            </Button>
-          ]}
-        >
-        <p class="message">{this.props.message}</p>
-        <p class="reason-text"> {this.props.reason} </p>
-        { (this.props.action === "Send Request") ? <textarea value={this.props.remarks || ""} class="remarks" onChange={this.props.onRemarksChange}/> : ''}
-        </Modal>
-      )
-    }
+          </Button>,
+        ]}
+      >
+        <p className="message">{this.props.message}</p>
+        <p className="reason-text"> {this.props.reason} </p>
+        {this.props.action === "Send Request" ? (
+          <textarea
+            value={this.props.remarks || ""}
+            class="remarks"
+            onChange={this.props.onRemarksChange}
+          />
+        ) : (
+          ""
+        )}
+      </Modal>
+    );
+  }
 }
 
 PromptModal.propTypes = {
@@ -44,5 +48,5 @@ PromptModal.propTypes = {
   message: PropTypes.string,
   buttonType: PropTypes.string,
   _action: PropTypes.string,
-  visible: PropTypes.bool
+  visible: PropTypes.bool,
 };
